b35ff1a3fd9ff437ae31bbeeb8b2727dc03f053a
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-06-13 22:13:36 -0400

    App Finish

---
+++ app/src/main/java/com/example/sportsapp/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.sportsapp;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int pos);
+}
--- app/src/main/java/com/example/sportsapp/MainActivity.java
+++ app/src/main/java/com/example/sportsapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.sportsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ItemClickListener {
+
+    private RecyclerView recyclerView;
+    private List<Sport> sportList;
+    private CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        sportList = new ArrayList<>();
+
+        Sport s1 = new Sport("Football", R.drawable.football);
+        Sport s2 = new Sport("Basketball", R.drawable.basketball);
+        Sport s3 = new Sport("Volleyball", R.drawable.volley);
+        Sport s4 = new Sport("Tennis", R.drawable.tennis);
+        Sport s5 = new Sport("Ping Pong", R.drawable.ping);
+
+        sportList.add(s1);
+        sportList.add(s2);
+        sportList.add(s3);
+        sportList.add(s4);
+        sportList.add(s5);
+
+        adapter = new CustomAdapter(sportList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view, int pos) {
+        Toast.makeText(this, "You chose: " + sportList.get(pos).sportName, Toast.LENGTH_SHORT).show();
     }
 }
